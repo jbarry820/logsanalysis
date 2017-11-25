3fc424d193018e66a65e8432def6705cb03ae7af
Jim Barry <jbarry@pelicom.net>
2017-11-25 06:47:51 -0600

    Add changed file for lines too long

--- logsanalysisdb.py
+++ logsanalysisdb.py
@@ -7,7 +7,8 @@ DBNAME = "news"
 def top_three_articles():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("SELECT path, COUNT(*) FROM log GROUP BY path ORDER BY COUNT DESC LIMIT 3")
+    c.execute("SELECT path, COUNT(*) FROM log GROUP BY path \
+      ORDER BY COUNT DESC LIMIT 3")
     rows = c.fetchall()
     db.close()
     return rows
@@ -16,7 +17,9 @@ def top_three_articles():
 def most_pop_authors():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("select authors.name, article_views.article_views from authors join article_views on authors.id = article_views.author")
+    c.execute("select authors.name, article_views.article_views \
+      from authors join article_views \
+      on authors.id = article_views.author")
     rows = c.fetchall()
     db.close()
     return rows
@@ -25,7 +28,8 @@ def most_pop_authors():
 def days_most_errors():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("select date, count(date) from status_date group by date order by count(date) desc")
+    c.execute("select date, count(date) from status_date \
+      group by date order by count(date) desc")
     rows = c.fetchall()
     db.close()
     return rows
